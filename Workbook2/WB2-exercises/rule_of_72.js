fc466ff397c822ff088c2f6694f76b8c0c0018e7
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-10-25 08:46:08 -0400

    added new files

--- Workbook2/WB2-exercises/rule_of_72.js
+++ Workbook2/WB2-exercises/rule_of_72.js
@@ -2,7 +2,7 @@ var num = 72;
 var percentageRate = 5; 
 var expectedYears = num / percentageRate;
 
-console.log("At an interest rate of" + percentageRate + 
-                " percent, any balance in the bank account will double " + 
+console.log("At an interest rate of " + percentageRate + 
+                " percent, any balance in the bank account will double" + 
                 " in " + expectedYears + 
                 " years ");
